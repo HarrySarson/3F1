4f44b2fe4a020b6af8baa23c105ccefcad6362dd
hds28 <hds28@tw304.eng.cam.ac.uk>
2017-11-20 12:48:32 +0000

    fixes script to generate pdf

--- save_pdfs.m
+++ save_pdfs.m
@@ -1,13 +1,18 @@
 files = arrayfun(@(fileinfo) string(fileinfo.name), dir(pwd));
 
+mkdir('./tmp');
 
 for i = 1:length(files)
     [a, name, ext] = fileparts(files(i));
     
     if (strcmp(ext, '.fig'))    
-        ouputname = strcat(pwd, '/figures/', name);
+        ouputname = strcat(pwd, '/tmp/', name);
         openfig(files(i));
         print(gcf, ouputname, '-bestfit', '-dpdf');
         close gcf;
     end
 end
+
+system('pdfunite ./tmp/* ./figures.pdf');
+
+rmdir('./tmp', 's');
